7e5593c29e33224c5b87cf84bbef488938759468
smakosh <ismai23l@hotmail.com>
2018-01-20 05:32:35 +0000

    simple weather app

---
+++ src/components/app.jsx
@@ -0,0 +1,148 @@
+import React, {Component} from 'react'
+import moment from 'moment'
+import axios from 'axios'
+import NProgress from 'react-nprogress'
+
+import 'react-nprogress/nprogress.css'
+import 'weather-icons/css/weather-icons.css'
+import 'unnamed'
+import '../assets/styles/main.css'
+import logo from '../assets/img/logo.svg'
+
+export default class App extends Component {
+    
+    state = {
+        City: '',
+        day: '',
+        temperature: '',
+        summary: '',
+        icon: 'night-clear'
+    }
+
+    maker = 'http://smakosh.com'
+
+    Convert = temp => `${((temp - 32) * 5/9).toFixed(2)} °C`
+
+    GetWeather = City => {
+
+        const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+        const geocodeURL = `http://maps.googleapis.com/maps/api/geocode/json?address=${City}`
+        return axios.get(proxyUrl + geocodeURL)
+        .then((response) => {
+            if(response.data.status === 'ZERO_RESULTS') {
+                throw new Error('Unable to find that City')
+            }
+            const lat = response.data.results[0].geometry.location.lat 
+            const lng = response.data.results[0].geometry.location.lng
+
+            const dataCity = {City}
+            const json = JSON.stringify(dataCity)
+            localStorage.setItem('dataCity', json)
+
+            const weatherURL = `https://api.darksky.net/forecast/${process.env.REACT_APP_FORCAST_API_KEY}/${lat},${lng}`
+            return axios.get(proxyUrl + weatherURL)
+        }).then((response) => {
+            
+            const day = moment.unix(response.data.currently.time).format("dddd");
+            const temperature = this.Convert(response.data.currently.temperature)
+            const summary = response.data.currently.summary
+            let icon = response.data.currently.icon
+
+            if(icon === 'clear-day') icon = 'day-sunny'
+            else if(icon === 'clear-night') icon = 'night-clear'
+            else if(icon === 'wind') icon = 'cloudy-windy'
+            else if(icon === 'partly-cloudy-day') icon = 'day-cloudy'
+            else if(icon === 'partly-cloudy-night') icon = 'night-alt-partly-cloudy'
+
+            this.setState(() => ({
+                City,
+                day,
+                temperature,
+                summary,
+                icon
+            }))
+        }).catch((e) => {
+            if(e.code === 'ENOTFOUND') console.log('unable to connect to API servers')
+            else console.log(e.message)
+        })
+    }
+
+    componentWillMount() {
+        try {
+
+            const json = localStorage.getItem('dataCity')
+            const dataCity = JSON.parse(json)
+            const CityLocal = dataCity.City
+
+            if (CityLocal) {
+                NProgress.start()
+                this.GetWeather(CityLocal)
+                NProgress.done()
+            }
+        } catch(e) {
+            console.log('LocalStorage is empty')
+        }
+    }
+
+    onSubmit = (e) => {
+        
+        e.preventDefault()
+        NProgress.start()
+        const FormCity = e.target.elements.city.value
+        if(!FormCity) {
+            alert('Please enter the name of a city!')
+        }
+        this.GetWeather(FormCity)
+        NProgress.done()
+    }
+    render() {
+        return (
+            <div className={`container-full app ${this.state.icon}`}>
+                <div className="valigned">
+                    <div className="container center-text">
+                        <div className={`card left-text ${this.state.icon}-clear`}>
+                            <header className="center-text">
+                                <img src={logo} className="App-logo" alt="logo" />
+                                <h2>Enter the name of a city down bellow</h2>
+                            </header>
+                            <div className="row">
+                                <div className="column xlarge-2 large-1 hide-tablet-down"></div>
+                                <div className="column xlarge-8 large-10 small-12">
+                                    <form onSubmit={this.onSubmit}>
+                                        <div className="input-field purple-input">
+                                            <span className="weather-icon"></span>
+                                            <input type="text" name="city" placeholder="Ex: London..." autoComplete="off"/>
+                                        </div>
+                                        <div className="center-text">
+                                            <button type="submit" className="btn">
+                                                Submit
+                                            </button>
+                                        </div>
+                                    </form>
+                                </div>
+                                <div className="column xlarge-2 large-1 hide-tablet-down"></div>
+                            </div>
+                            { this.state.City.length > 0 ?
+                                <div className="center-text result">
+                                    <h1 className="city">{this.state.City}</h1>
+
+                                    <h2>Today is {this.state.day} & currently it's:</h2>
+
+                                    <h1>{this.state.temperature}</h1>
+
+                                    <h2><i className={`wi wi-${this.state.icon}`}></i> The weather is {this.state.summary}</h2>
+                                </div>
+                                : null
+                            }
+                        </div>
+                        <div className="center-text maker">
+                            <h4>
+                                Made with <i className="fa fa-heartbeat"></i> By <a href={this.maker} target="_blank">Smakosh</a>
+                            </h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
